e417a17ae209ffd4afa2adcdccdae80574f41d58
Haithem Bel Haj <haythem.belhaj@gmail.com>
2015-09-16 22:36:00 +0200

    improve routing

--- index.js
+++ index.js
@@ -16,46 +16,79 @@ app.use(bodyParser.urlencoded({
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+// index
 app.get('/', function(req, res){
 
   res.render('index', {data: synq.get()})
 });
 
-app.post('/get', function(req, res){
+// rest
+app.post('/get', get);
+app.post('/start', validate(start));
+app.post('/stop', validate(stop));
+app.post('/remove', validate(remove));
 
-  res.json(synq.get(req.body.url));
+// start
+app.listen(5000, function () {
+
+  var addr = server.address();
+
+  console.log('Example app listening at http://%s:%s', addr.host, addr.port);
 });
 
+// export for testing
+module.exports = app;
 
-app.post('/start', function(req, res){
+/**
+ * functions
+ */
+
+function get(req, res){
+
+  var data = synq.get(req.body.url);
+
+  if(!data)
+    res.status(404);
+
+  res.json(data);
+}
+
+function start(req, res){
 
   synq.start(req.body.url, function(instance){
 
     res.json(instance);
   });
-});
+}
 
-app.post('/stop', function(req, res){
+function stop(req, res){
 
   synq.stop(req.body.url);
 
-  res.send('ok');
-});
+  return get(req, res);
+}
+
+function remove(req, res){
+
+  synq.remove(req.body.url);
+
+  delete req.body.url;
+
+  return get(req, res);
+}
 
 
-app.post('/remove', function(req, res){
+function validate(next){
 
-  synq.remove(req.box.url);
+  return function(req, res){
 
-  res.send('ok');
-});
+    if(req.body.url)
+      return next(req, res);
 
-var server = app.listen(5000, function () {
+    res.status(400);
+    res.send('Please Provide an URL');
+  }
+}
 
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
 
 
