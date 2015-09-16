b07d19620ed96c9ded5468df96cae541c95d6b6a
Steffen Dietz <sd@dietaikonauten.com>
2015-09-16 18:07:37 +0200

    :art: change port to 1337

--- index.js
+++ index.js
@@ -50,12 +50,10 @@ app.post('/remove', function(req, res){
   res.send('ok');
 });
 
-var server = app.listen(5000, function () {
+var server = app.listen(1337, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
-
